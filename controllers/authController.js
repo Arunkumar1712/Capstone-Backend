1a9b2840928995e0976f219880cb4ad60d974ff5
Arunkumar1712 <arunk05691@gail.com>
2024-04-04 13:18:57 +0530

    updatwe

--- controllers/authController.js
+++ controllers/authController.js
@@ -60,6 +60,7 @@ const loginUser =async (req,res)=>{
        jwt.sign({email: user.email, id: user._id,name:user.name},process.env.JWT_SECRET,{},(err,token)=>{
         if(err) throw err;
         res.cookie('token',token).json(user)
+        localStorage.setItem("token",token);//we can remove any time
        } )
         }
         if(!match){
