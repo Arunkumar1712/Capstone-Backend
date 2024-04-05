6fdea2bbb2d18ce62af010ca287f12b59f53adb1
Arunkumar1712 <arunk05691@gail.com>
2024-04-05 06:58:06 +0530

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,7 +10,7 @@ router.use(express.json())
 router.use(cookieParser())
 router.use(express.urlencoded({extended: false}))
 const corsOptions = {
-    origin: ['https://660ecc4e96df2719848f6a79--genuine-crostata-3ec8e9.netlify.app', 'http://localhost:3000'], // Update with your Netlify origin
+    origin: ['https://outfit-oracle.netlify.app/', 'http://localhost:3000'], // Update with your Netlify origin
     credentials: true,
   };
   
